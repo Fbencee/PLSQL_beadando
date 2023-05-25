1410338d5cf0493549c8ec6d0329fe7f8e5e161c
Fbencee <79247037+Fbencee@users.noreply.github.com>
2023-05-25 12:05:21 +0200

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,1 +1,1 @@
-mentoszolgalat.ujhivas(1,TO_TIMESTAMP(TO_CHAR(SYSDATE, 'MM-DD-YYYY HH24:MI:SS'),'MM-DD-YYYY HH24:MI:SS','NLS_DATE_LANGUAGE=AMERICAN'),'Kuruc László', 'Eper utca 12.', 'ájult beteg');
+ insert into hivas values(2,TO_TIMESTAMP(TO_CHAR(SYSDATE, 'MM-DD-YYYY HH24:MI:SS'),'MM-DD-YYYY HH24:MI:SS','NLS_DATE_LANGUAGE=AMERICAN'),'Kuruc László', 'Eper utca 12.', 'ájult beteg', 1);
