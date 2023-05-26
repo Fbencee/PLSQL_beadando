ce2dcad14e1aa72b2a6e58a5e59c915b18975447
Fbencee <79247037+Fbencee@users.noreply.github.com>
2023-05-26 15:23:01 +0200

    Update Naplo_trigger.sql

--- Naplo_trigger.sql
+++ Naplo_trigger.sql
@@ -5,10 +5,12 @@ SELECT * FROM (SELECT hivasId FROM HIVAS ORDER BY idopont desc) WHERE ROWNUM <= 
 CREATE OR REPLACE TRIGGER feltoltnaplo AFTER insert ON HIVAS 
 DECLARE
 sor hivas%ROWTYPE;
+minutes int;
 BEGIN
  SELECT * INTO sor FROM HIVAS WHERE hivasId = (SELECT * FROM (SELECT hivasId FROM HIVAS ORDER BY idopont desc) WHERE ROWNUM <= 1);
+ minutes := MOD(ABS(DBMS_RANDOM.RANDOM()), 20) + 5;
  IF (sor.mentotkuld >= 1) then
-  INSERT INTO NAPLO VALUES (naplo_sqe.nextval, SYSDATE, (SELECT USER FROM DUAL), sor.hivasId);
+  INSERT INTO NAPLO VALUES (naplo_sqe.nextval, sysdate + (1/1440)*minutes, (SELECT USER FROM DUAL), sor.hivasId);
  END IF;
 END;
 
