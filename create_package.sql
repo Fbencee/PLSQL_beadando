df08ac93a358587c63a11dad99dec0d9de5d51e3
Fbencee <79247037+Fbencee@users.noreply.github.com>
2023-05-26 21:03:11 +0200

    Update create_package.sql

--- create_package.sql
+++ create_package.sql
@@ -1,8 +1,8 @@
-
 create or replace PACKAGE mentoszolgalat is
   PROCEDURE FELTOLTLALK;
   PROCEDURE FELTOLTAUTO;
   PROCEDURE UJHIVAS(p_id number, p_idopont date, p_hivo varchar2, p_helyszin varchar2, p_leiras varchar2, p_mentotkuld int);
+  PROCEDURE BETEG_FELTOLT;
   FUNCTION RAND_B return VARCHAR2;
   FUNCTION RAND_TIP return VARCHAR2;
 end;
@@ -66,5 +66,34 @@ create or replace PACKAGE BODY mentoszolgalat is
     when others then
     dbms_output.put_line('Valami hiba történt!');
    end;
+
+   procedure beteg_feltolt as
+    F UTL_FILE.FILE_TYPE;
+    sor varchar2(200);
+    c1 varchar(30);
+    c2 varchar(30);
+    c3 varchar(30);
+    c4 varchar(30);
+   begin
+    F := UTL_FILE.FOPEN('DOP','betegek.txt','R');
+     loop
+       UTL_FILE.GET_LINE(F,sor,100);
+
+       c1 := SUBSTR(sor, 1, INSTR(sor, ',') - 1);
+       sor := SUBSTR(sor, INSTR(sor, ',') + 1);
+
+       c2 := SUBSTR(sor, 1, INSTR(sor, ',') - 1);
+       sor := SUBSTR(sor, INSTR(sor, ',') + 1);
+
+       c3 := SUBSTR(sor, 1, INSTR(sor, ',') - 1);
+       c4 := SUBSTR(sor, INSTR(sor, ',') + 1);
+
+       INSERT INTO beteg VALUES(beteg_sqe.nextval,c1,c2,c3,c4);
+     end loop;
+   UTL_FILE.FCLOSE(F);
+  exception
+    when others then 
+       DBMS_OUTPUT.put_line('Nem sikerült a feltöltés!');
+  end;
 
 END;
