331b5159f61c8f37b10b719f6d284f752ebd3e53
Fbencee <79247037+Fbencee@users.noreply.github.com>
2023-05-28 22:55:44 +0200

    Update select_call_count.sql

--- select_call_count.sql
+++ select_call_count.sql
@@ -1,4 +1,6 @@
-CREATE OR REPLACE FUNCTION hivasokszama(p_datum varchar2(20)) RETURN NUMBER is
+CREATE OR REPLACE FUNCTION hivasokszama(p_datum varchar2) RETURN NUMBER is
+ hivasok NUMBER;
 BEGIN
-  SELECT COUNT(*) FROM HIVAS WHERE idopont > TO_TIMESTAMP(p_datum,'YYYY/MM/DD') AND idopont < TO_TIMESTAMP(p_datum,'YYYY/MM/DD');
+ SELECT COUNT(*) INTO hivasok FROM HIVAS WHERE idopont > TO_TIMESTAMP(p_datum,'YYYY/MM/DD') AND idopont < (TO_TIMESTAMP(p_datum, 'YYYY/MM/DD') + interval '1' day);
+ RETURN hivasok;
 END;
