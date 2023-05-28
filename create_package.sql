44b56f22e8581111e236041cf953656cc2bfbe8c
Fbencee <79247037+Fbencee@users.noreply.github.com>
2023-05-28 23:50:33 +0200

    Update create_package.sql

--- create_package.sql
+++ create_package.sql
@@ -3,8 +3,12 @@ create or replace PACKAGE mentoszolgalat is
   PROCEDURE FELTOLTAUTO;
   PROCEDURE UJHIVAS(p_id number, p_idopont date, p_hivo varchar2, p_helyszin varchar2, p_leiras varchar2, p_mentotkuld int);
   PROCEDURE BETEG_FELTOLT;
+  PROCEDURE hivasok_beteg(p_beteg varchar2);
+  PROCEDURE hivasok_datum(p_idopont1 varchar2, p_idopont2 varchar2);
+  PROCEDURE hivasok(p_hivo varchar2);
   FUNCTION RAND_B return VARCHAR2;
   FUNCTION RAND_TIP return VARCHAR2;
+  FUNCTION hivasokszama(p_datum varchar2) RETURN NUMBER;
 end;
 
 create or replace PACKAGE BODY mentoszolgalat is
@@ -95,5 +99,61 @@ create or replace PACKAGE BODY mentoszolgalat is
     when others then 
        DBMS_OUTPUT.put_line('Nem sikerült a feltöltés!');
   end;
+
+  PROCEDURE hivasok(p_hivo varchar2) is
+  BEGIN
+    FOR hivasok IN (SELECT * FROM hivas WHERE hivo = p_hivo) LOOP
+      DBMS_OUTPUT.PUT_LINE('Hívás ID: ' || hivasok.hivasId);
+      DBMS_OUTPUT.PUT_LINE('Hívás dátuma: ' || hivasok.idopont);
+      DBMS_OUTPUT.PUT_LINE('Hívó neve: ' || hivasok.hivo);
+      DBMS_OUTPUT.PUT_LINE('Hívás helyszine: ' || hivasok.helyszin);
+      DBMS_OUTPUT.PUT_LINE('Hívás leírása: ' || hivasok.leiras);
+      IF hivasok.mentotkuld = 1 THEN
+       DBMS_OUTPUT.PUT_LINE('Mentő kiküldve.');
+      ELSE
+       DBMS_OUTPUT.PUT_LINE('Nincs mentő küldve.');
+      END IF;
+      DBMS_OUTPUT.PUT_LINE('-------------------');
+    END LOOP;
+  END;
+
+  PROCEDURE hivasok_datum(p_idopont1 varchar2, p_idopont2 varchar2) is
+  BEGIN
+   FOR hivasok IN (SELECT * FROM hivas WHERE idopont > TO_TIMESTAMP(p_idopont1,'YYYY/MM/DD') AND idopont < TO_TIMESTAMP(p_idopont2, 'YYYY/MM/DD')) LOOP
+    DBMS_OUTPUT.PUT_LINE('Hívás ID: ' || hivasok.hivasId);
+    DBMS_OUTPUT.PUT_LINE('Hívás dátuma: ' || hivasok.idopont);
+    DBMS_OUTPUT.PUT_LINE('Hívó neve: ' || hivasok.hivo);
+    DBMS_OUTPUT.PUT_LINE('Hívás helyszine: ' || hivasok.helyszin);
+    DBMS_OUTPUT.PUT_LINE('Hívás leírása: ' || hivasok.leiras);
+    IF hivasok.mentotkuld = 1 THEN
+     DBMS_OUTPUT.PUT_LINE('Mentő kiküldve.');
+    ELSE
+     DBMS_OUTPUT.PUT_LINE('Nincs mentő küldve.');
+    END IF;
+    DBMS_OUTPUT.PUT_LINE('-------------------');
+   END LOOP;
+  END;
+
+  PROCEDURE hivasok_beteg(p_beteg varchar2) IS
+  BEGIN
+   FOR adatok IN (SELECT h.hivasId, h.idopont, b.nev, b.tajszam, b.diagnozis
+                 FROM hivas h
+                 JOIN beteg b ON h.hivasId = b.hivasId
+                 WHERE b.nev = p_beteg) LOOP
+    DBMS_OUTPUT.PUT_LINE('Hívás ID: ' || adatok.hivasId);
+    DBMS_OUTPUT.PUT_LINE('Hívás dátuma: ' || adatok.idopont);
+    DBMS_OUTPUT.PUT_LINE('Beteg neve: ' || adatok.nev);
+    DBMS_OUTPUT.PUT_LINE('Beteg tajszáma: ' || adatok.tajszam);
+    DBMS_OUTPUT.PUT_LINE('Beteg diagnózis: ' || adatok.diagnozis);
+   END LOOP;
+  END;
+
+  FUNCTION hivasokszama(p_datum varchar2) RETURN NUMBER is
+   hivasok NUMBER;
+  BEGIN
+   SELECT COUNT(*) INTO hivasok FROM HIVAS WHERE idopont > TO_TIMESTAMP(p_datum,'YYYY/MM/DD') AND idopont < (TO_TIMESTAMP(p_datum, 'YYYY/MM/DD') + interval '1' day);
+   RETURN hivasok;
+  END;
+  
 
 END;
